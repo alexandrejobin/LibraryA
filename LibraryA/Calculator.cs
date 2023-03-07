0a5286d9084e691620fb059447aed3584f81d824
Alexandre Jobin <alexandre.jobin@cnesst.gouv.qc.ca>
2023-03-07 13:58:47 -0500

    first commit

---
+++ LibraryA/Calculator.cs
@@ -0,0 +1,16 @@
+namespace LibraryA
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Permet d'additionner la valeur1 à la valeur2.
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
